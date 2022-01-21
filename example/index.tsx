5e5042f9e9f49e5640343e73a51a6b92dcaf3ba1
ivantabera <57042047+ivantabera@users.noreply.github.com>
2022-01-20 18:59:22 -0600

    Revert "paso 1 al 5"
    This reverts commit e0564b6910409ca5adb40597d615fecb09f074a7.

--- example/index.tsx
+++ example/index.tsx
@@ -1,36 +1,13 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { ProductCard, ProductImage, ProductTitle, ProductButtons } from '../.';
-
-const product = {
-  id:'1',
-  title:'coffe mug',
-  // img:'./coffee-mug.png'
-}
+import { Thing } from '../.';
 
 const App = () => {
   return (
-    <>
-      <ProductCard
-          product={product}
-          initialValues={{
-              count:4,
-              maxCount:10
-          }}
-      >
-          {
-              ({ count, isMaxCountReached, maxCount, increaseBy, reset }) => (
-                  <>
-                      <ProductImage />
-                      <ProductTitle />
-                      <ProductButtons />
-                  </>
-              )
-          }
-          
-      </ProductCard>
-    </>
+    <div>
+      <Thing />
+    </div>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,6 @@
-export * from './components';
+import * as React from 'react';
+
+// Delete me
+export const Thing = () => {
+  return <div>the snozzberries taste like snozzberries</div>;
+};
